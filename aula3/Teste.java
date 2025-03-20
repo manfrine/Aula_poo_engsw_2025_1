41910eba681fa9e43804e41009eae6cd9ad3e96d
Manfrine Santos <manfrine20@yahoo.com.br>
2025-03-19 20:53:33 -0400

    Classe finalizada

--- aula3/Teste.java
+++ aula3/Teste.java
@@ -31,6 +31,7 @@ public class Teste {
 			System.out.println("A marca 2: "+m2.nome);
 			System.out.println("A marca 3: "+m3.nome);
 			
+			System.out.println("Escolha a marca pelo número correspondente ");
 			b = into.nextInt();
 			if(b==1)
 				vei.marca = m1;
@@ -39,12 +40,41 @@ public class Teste {
 			else
 				vei.marca = m3;
 			
+			System.out.println("\n------------------------\n");
 			
-			System.out.println("O veículo: "+vei.modelo);
+			System.out.println("\nDigite o modelo da moto: ");
+			m.modelo = into.next();
+			System.out.println("Digite a cor do moto: ");
+			m.cor = into.next();
+			System.out.println("Digite o valor do moto: ");
+			m.valor = into.nextDouble();
+			System.out.println("Digite as cilindradas do moto: ");
+			m.cilindradas = into.nextInt();
+			m.calcIpva();// mensagem
+			
+			System.out.println("\nA marca 1: "+m1.nome);
+			System.out.println("A marca 2: "+m2.nome);
+			System.out.println("A marca 3: "+m3.nome);
+			
+			System.out.println("Escolha a marca pelo número correspondente ");
+			c = into.nextInt();
+			if(c==1)
+				m.marca = m1;
+			if(c==2)
+				m.marca = m2;
+			else
+				m.marca = m3;
+						
+			System.out.println("\nO veículo: "+vei.modelo);
 			System.out.println("O valor veículo: R$ "+vei.valor);
 			System.out.println("O valor do IPVA veículo: R$ "+vei.ipva);
-			System.out.println("A marca veículo: R$ "+vei.marca.nome);
-			
+			System.out.println("A marca do veículo: "+vei.marca.nome);
+			System.out.println("\n------------------------\n");
+			System.out.println("\nA moto: "+m.modelo);
+			System.out.println("O valor da moto: R$ "+m.valor);
+			System.out.println("O valor do IPVA da moto: R$ "+m.ipva);
+			System.out.println("As cilindradas da moto: R$ "+m.cilindradas);
+			System.out.println("A marca do moto: "+m.marca.nome);
 			
 			
 			System.out.println("\nDigite 1 p/ continuar ou 0 p/ sair: ");
